2bf94d096ee715631ae41858c64525d57ca62aa9
thehimel <60639301+thehimel@users.noreply.github.com>
2023-11-11 00:02:37 +0100

    Define variables

---
+++ variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const fruitName string = "Apple"
+
+func main() {
+	fmt.Println(fruitName)
+
+	digit := 100
+	fmt.Println(digit)
+
+	var word = "Simple"
+	fmt.Println(word)
+	fmt.Printf("Variable type: %T.\n", word)
+}
