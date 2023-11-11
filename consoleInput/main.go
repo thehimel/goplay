d4b1db17992763a5da9bf3b05605df90cd807849
thehimel <60639301+thehimel@users.noreply.github.com>
2023-11-11 11:59:39 +0100

    updated consoleInput

--- consoleInput/main.go
+++ consoleInput/main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Print("Enter input: ")
 	data, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Input error!")
+		fmt.Println("Error reading from console!")
 		return
 	}
 	fmt.Println("Entered data:", data)
