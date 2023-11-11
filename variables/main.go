a08def74c9801397518b9078ab92d845b75ca0e0
Himel Das <60639301+thehimel@users.noreply.github.com>
2023-11-11 12:06:50 +0100

    updated variables

--- variables/main.go
+++ variables/main.go
@@ -13,4 +13,7 @@ func main() {
 	var word = "Simple"
 	fmt.Println(word)
 	fmt.Printf("Variable type: %T.\n", word)
+
+	var num int64 = 10000000000
+	fmt.Printf("Value = %v, Type = %T", num, num)
 }
