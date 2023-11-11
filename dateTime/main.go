2dff6ac406643aa8f563961ea052403e54eb9caa
Himel Das <60639301+thehimel@users.noreply.github.com>
2023-11-11 20:11:12 +0100

    updated dateTime

--- dateTime/main.go
+++ dateTime/main.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	fmt.Println("Now =", time.Now())
 	createdMoment := time.Date(2023, time.December, 10, 14, 44, 22, 8, time.UTC)
+
 	fmt.Println("Custom Moment =", createdMoment)
 	fmt.Println("Custom Moment =", createdMoment.Format(time.DateTime))
+
+	customDate := "2023-12-10 14:44:22"
+	parsedDate, _ := time.Parse(time.DateTime, customDate)
+	fmt.Printf("Parsed date = %v, data type = %T", parsedDate, parsedDate)
 }
